34ff7cb250dcf96f46b70d48ba2be0133c82a33d
vijayaraagavan <svijayaraagavan@gmail.com>
2022-04-29 14:32:10 +0530

    diff btw channel buffer and unbuffered

--- cmd/simple-service/main.go
+++ cmd/simple-service/main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 )
+var y int =0
+func test(x chan int) {
+	for i:=0; i<1000000; i++ {
+		y += i
+	}
+	data := <- x
+	fmt.Println(data,y)
+	fmt.Println(x)
+}
 func main(){
-	fmt.Println("hello")
-	fmt.Println("world")
-	fmt.Println("nothing")
+	data := make(chan int,3);
+	// fmt.Printf("%T",data)
+	go test(data)
+	go test(data)
+	data <- 10
+	// data <- 15
+	// x := <- data
+	// go test(data)
+	fmt.Println("main ends")
 }
 // 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
