f724cab7a87e8e57059eeaa3c6dd5947d959ea70
vijayaraagavan <svijayaraagavan@gmail.com>
2022-04-29 19:04:16 +0530

    understanding channel

--- cmd/simple-service/main.go
+++ cmd/simple-service/main.go
@@ -2,25 +2,42 @@ package main
 
 import (
 	"fmt"
+	// "time"
+	"log"
 )
-var y int =0
-func test(x chan int) {
-	for i:=0; i<1000000; i++ {
-		y += i
+func count(a,b int, ch chan string) {
+	fmt.Println("waiting: ",<-ch)
+	fmt.Println("enter data send")
+	for i:=a; i<b; i++ {
+		log.Println(i)
 	}
-	data := <- x
-	fmt.Println(data,y)
-	fmt.Println(x)
+	// ch <- "hello"
+	// time.Sleep(time.Second + 1)
+	// fmt.Println("ending")
 }
-func main(){
-	data := make(chan int,3);
-	// fmt.Printf("%T",data)
-	go test(data)
-	go test(data)
-	data <- 10
-	// data <- 15
-	// x := <- data
-	// go test(data)
-	fmt.Println("main ends")
+
+func cou(a,b int, ch chan string) {
+	fmt.Println("enter data send")
+	for i:=a; i<b; i++ {
+		log.Println(i)
+		if i==450 {
+			ch <- "ok"
+		}
+	}
+	// ch <- "hello"
+	// time.Sleep(time.Second + 1)
+	// fmt.Println("ending")
+}
+
+func main() {
+	var data = make(chan string)
+	go cou(0,500,data)
+	count(51,100,data)
+	// time.Sleep(time.Second +2)
+	// fmt.Println(res)
+	// fmt.Println("enter data request")
+	// fmt.Println(<-data)
+	// time.Sleep(time.Second)
+
 }
 // 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
